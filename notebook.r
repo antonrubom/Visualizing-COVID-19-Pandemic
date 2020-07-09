126db3e45dca7af8450b7b781bded99b87b64b0d
Tony Rubombora <tonyrubombora@gmail.com>
2020-07-09 13:47:02 +0300

    Added 5th and 6th analysis

--- notebook.r
+++ notebook.r
@@ -63,3 +63,7 @@ ggplot(china_after_feb15, aes(x=date,y=cum_cases)) +
 geom_line()+
 geom_smooth(method="lm", se=FALSE) +
 ylab("Cumulative confirmed cases")
+# Filter confirmed_cases_china_vs_world for not China
+
+#Adding 6th Analysis
+# Filter confirmed_cases_china_vs_world for not China not_china <- confirmed_cases_china_vs_world %>% filter(is_china ==Not China) # Using not_china, draw a line plot cum_cases vs. date # Add a smooth trend line using linear regression, no error bars plt_not_china_trend_lin <- ggplot(not_china, aes(x=date,y=cum_cases)) + geom_line() + geom_smooth(method=lm, se=FALSE) + ylab(Cumulative confirmed cases) # See the result plt_not_china_trend_lin
